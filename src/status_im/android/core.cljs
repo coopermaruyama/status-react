44fdea6a8ae6d7e373a36af586b4d3bbb3012f9e
Gustavo Nunes <gustavonunesedl@gmail.com>
2017-03-30 02:39:37 -0300

    created transaction details screen

--- src/status_im/android/core.cljs
+++ src/status_im/android/core.cljs
@@ -23,6 +23,7 @@
             [status-im.accounts.recover.screen :refer [recover]]
             [status-im.accounts.screen :refer [accounts]]
             [status-im.transactions.screen :refer [confirm]]
+            [status-im.transactions.screens.transaction-details :refer [transaction-details]]
             [status-im.chats-list.screen :refer [chats-list]]
             [status-im.new-chat.screen :refer [new-chat]]
             [status-im.new-group.screen-public :refer [new-public-group]]
@@ -150,6 +151,7 @@
                                        :qr-scanner qr-scanner
                                        :qr-code-view qr-code-view
                                        :confirm confirm
+                                       :transaction-details transaction-details
                                        :contact-list-modal contact-list-modal)]
                        [component])]])]]))))})))
 
